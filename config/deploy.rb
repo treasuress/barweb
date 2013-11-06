a58ec03e37daf9e3c422ff9e2b22780102e2c2e6
auroral <pessoa.a.feliz@gmail.com>
2013-11-05 22:33:04 -0200

    Configuração para produção

---
+++ config/deploy.rb
@@ -0,0 +1,33 @@
+require "bundler/capistrano"
+
+load "config/recipes/base"
+load "config/recipes/nginx"
+load "config/recipes/unicorn"
+load "config/recipes/sqlite"
+load "config/recipes/nodejs"
+load "config/recipes/rvm"
+load "config/recipes/check"
+
+#server "177.71.144.131", :web, :app, :db, primary: true
+
+set :user, "deployer"
+set :application, "barweb"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+set :scm, "git"
+set :repository, "git@github.com:treasuress/#{application}.git"
+set :branch, "master"
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
+
+namespace :deploy do
+  desc "reload the database with seed data"
+  task :seed do
+    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+  end
+end
