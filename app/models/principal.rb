845e360f8b0cd7a8485a8731b5cf379a7b1affd9
auroral <pessoa.a.feliz@gmail.com>
2013-11-19 00:21:44 -0200

    retirada de tamanhos de imagens que não serão usados - principal

--- app/models/principal.rb
+++ app/models/principal.rb
@@ -7,35 +7,33 @@ class Principal < ActiveRecord::Base
 
   validates_attachment_presence :logo_image, :gallery_image, :events_image, :contact_image, :follow_fb_image, :follow_tw_image, :all_homes_image
 
-  #todo now - mudar nome do campos pra logo
   #todo now - colokr tamanho certo da logo
   has_attached_file :logo_image,
                     :styles => {:medium => "400x300#", :thumb => "100x100#"},
                     :url => "/assets/principals/:id/images/logo_:style.:extension",
                     :path => ":rails_root/public/assets/principals/:id/images/logo_:style.:extension"
-  #todo now - tirar tamanhos grands dessas imgs jah q n vai usar
   has_attached_file :gallery_image,
-                    :styles => {:medium => "400x300#", :thumb => "100x100#"},
+                    :styles => {:thumb => "100x100#"},
                     :url => "/assets/principals/:id/images/gallery_:style.:extension",
                     :path => ":rails_root/public/assets/principals/:id/images/gallery_:style.:extension"
   has_attached_file :events_image,
-                    :styles => {:medium => "400x300#", :thumb => "100x100#"},
+                    :styles => {:thumb => "100x100#"},
                     :url => "/assets/principals/:id/images/events_:style.:extension",
                     :path => ":rails_root/public/assets/principals/:id/images/events_:style.:extension"
   has_attached_file :contact_image,
-                    :styles => {:medium => "400x300#", :thumb => "100x100#"},
+                    :styles => {:thumb => "100x100#"},
                     :url => "/assets/principals/:id/images/contact_:style.:extension",
                     :path => ":rails_root/public/assets/principals/:id/images/contact_:style.:extension"
   has_attached_file :follow_fb_image,
-                    :styles => {:medium => "400x300#", :thumb => "100x45#"},
+                    :styles => {:thumb => "100x45#"},
                     :url => "/assets/principals/:id/images/follow_fb_:style.:extension",
                     :path => ":rails_root/public/assets/principals/:id/images/follow_fb_:style.:extension"
   has_attached_file :follow_tw_image,
-                    :styles => {:medium => "400x300#", :thumb => "100x45#"},
+                    :styles => {:thumb => "100x45#"},
                     :url => "/assets/principals/:id/images/follow_tw_:style.:extension",
                     :path => ":rails_root/public/assets/principals/:id/images/follow_tw_:style.:extension"
   has_attached_file :all_homes_image,
-                    :styles => {:medium => "400x300#", :thumb => "100x45#"},
+                    :styles => {:thumb => "100x45#"},
                     :url => "/assets/principals/:id/images/all_homes_:style.:extension",
                     :path => ":rails_root/public/assets/principals/:id/images/all_homes_:style.:extension"
 
