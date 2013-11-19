9c22c1a4ffe2db059444877e105b6f25e8f54a0c
auroral <pessoa.a.feliz@gmail.com>
2013-11-18 23:39:47 -0200

    correção teste - category

--- spec/support/model/category.rb
+++ spec/support/model/category.rb
@@ -1,6 +1,7 @@
 def new_category_hash
   puts IMAGE_EXAMPLE
   {
+      :active => true,
       :name => "Bares ",
       :description => "Todos os Bares",
       :main_image => File.new(IMAGE_EXAMPLE)
