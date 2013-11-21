7ee79844fa52bca17e97fcb3a97f3a25a8042372
auroral <pessoa.a.feliz@gmail.com>
2013-11-21 14:13:10 -0200

    otimização de busca de todos os estabelecimentos ativos

--- app/controllers/home_bar_controller.rb
+++ app/controllers/home_bar_controller.rb
@@ -4,6 +4,7 @@ class HomeBarController < ApplicationController
     @home_bar = HomeBar.find_by_name(params[:name])
 
 
+    #todo now - ver se dah pra colokr em application
     #todo now - mt errado, arrumar
     @home_bar_country = Country.getCountryName
     #@home_bar_country = Country.getCountryName(params[:id],params[:id])
@@ -15,12 +16,13 @@ class HomeBarController < ApplicationController
   end
 
   def all
+    @home_all_active_bars = HomeBar.active
+
     #todo futuro - colokr busca aleatoria aqui qndo for colokr varios tipos d ordenacao
 
     #todo futuro - buscar apenas por categorias ativas
     #Busca estabelecimentos - ordem alfabética
-    @home_bars = HomeBar.active
-    @home_bars = @home_bars.all.sort! { |a, b| a['name'].downcase <=> b['name'].downcase }
+    @home_bars = @home_all_active_bars.all.sort! { |a, b| a['name'].downcase <=> b['name'].downcase }
   end
 
 end
