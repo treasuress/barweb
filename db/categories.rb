620d45cd79b931e2061f859c8fbbda3a8b395045
auroral <pessoa.a.feliz@gmail.com>
2013-11-21 13:48:35 -0200

    adição de script para criação de categorias

---
+++ db/categories.rb
@@ -0,0 +1,12 @@
+def create_categories
+  begin
+
+    if Category.all.size == 0
+
+      Category.create([
+                          {:active => true, :name => "Bares", :description => "Todos os Bares", :category_image => File.open('public/categories/category1.jpg') },
+                      ])
+    end
+
+  end
+end
