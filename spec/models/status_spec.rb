6daf170772dbce87d153afd20becb15431862c51
auroral <laura@ANG3L-PROJS.(none)>
2013-11-22 19:36:29 -0200

    testes model

--- spec/models/status_spec.rb
+++ spec/models/status_spec.rb
@@ -7,6 +7,12 @@ describe Status do
     Status.count().should == 1
   end
 
+  it "error on create duplicate status" do
+    Status.create(new_status_hash)
+    Status.create(new_status_hash)
+    Status.count().should == 1
+  end
+
   it "error on create status" do
     Status.create({:name => nil})
     Status.count().should == 0
