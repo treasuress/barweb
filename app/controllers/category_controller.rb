27f3ed5ea83dcc60330a3253f3e34bebef92c3fc
auroral <laura@ANG3L-PROJS.(none)>
2013-11-26 01:20:44 -0200

    ajustes

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -2,20 +2,14 @@ class CategoryController < ApplicationController
 
   def index
     get_active_categories
-
-
   end
 
-
   def show
-    #todo now - aqui, colokr pra mostrar tdas as categorias e na pagina detalhe dessa, mostrar todos os estabelecimentos da categoria escolhida
     #todo futuro - colokr busca aleatoria aqui qndo for colokr varios tipos d ordenacao
-
+    #todo now - buscar apenas estabelecimentos da categoria certa
     #todo futuro - buscar apenas por categorias ativas
     #Busca estabelecimentos - ordem alfabética
     @home_bars = get_active_bars.all.sort! { |a, b| a['name'].downcase <=> b['name'].downcase }
-
-
 
     get_current_city
     get_current_category(@home_bars.first.category_id)
