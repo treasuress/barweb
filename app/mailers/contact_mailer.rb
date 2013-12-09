92f936998d1033c97b48cdd73adf198ac0165965
auroral <pessoa.a.feliz@gmail.com>
2013-12-09 15:58:59 -0200

    envio de email caso exiba estabelecimento que não devia ser exibido

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -4,7 +4,12 @@ class ContactMailer < ActionMailer::Base
 
   def contact_message(contact)
     @contact = contact
+    #todo rodar - colokr seu email pra poder ver resultado
     mail(:to => 'siac.bar@gmail.com', :subject => @contact['subject'])
+  end
+
+  def alert_when_error_should_never_happen(comment)
+    mail(:to => "lsiac.bar@gmail.com", :subject => "ERRO - " + comment, :body => comment)
   end
 
 end
