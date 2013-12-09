0a4f87326aa43fd9b0941567a2a12603e5490932
auroral <pessoa.a.feliz@gmail.com>
2013-12-09 16:00:10 -0200

    validacao - contato

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,9 +6,7 @@ class Contact
   attr_accessor :email, :message, :name, :subject
 
   validates :name, :length => {:in => 2..50}
-  #todo now - colokr qntidad minima d caracteres e colokr msg d erro pra isso
-  #validates :message, :length => {:in => 1..750}
-  validates :message, :length => {:minimum => 10, :maximum =>750, :message => "A mensagem deve possuir no minimo 10 caracteres!"}
+  validates :message, :length => {:in => 10..750}
 
   def initialize(attributes = {})
     attributes.each do |name, value|
