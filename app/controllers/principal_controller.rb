d1893d0e441d09d16ece15d3c5d7f63b7d497b5e
auroral <laura@ANG3L-PROJS.(none)>
2014-06-15 01:41:47 -0300

    correção de busca de cidade para mapa

--- app/controllers/principal_controller.rb
+++ app/controllers/principal_controller.rb
@@ -16,11 +16,6 @@ class PrincipalController < ApplicationController
     #todo futuro - buscar todos os bares ATIVOS que contem o que a pessoa digitar
     #todo futuro - colokr no fim dos resultados - link para lista completa de bares e criar uma otra pagina pra mostrar todos os resultados
     #@bar_name = HomeBar.find_by_name(params[:name])
-
-    #todo now - preciso pegar tdos os ids de cidades, como faz? - acho q tem q fazer a busca junto com a q busca os bares pra ser cidad do bar certo
-    #todo now - acho q pod usar a mesma busca do home bars, mas sem a part d ordenacao e limite - ou seja, fazer a busca generica separada
-    get_current_city_from_bar(@all_active_bars.first.city_id)
-
   end
 
   def contact
